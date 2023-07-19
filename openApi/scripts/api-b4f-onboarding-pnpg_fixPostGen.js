4346166709bcca2e2bdc311d508f005e44f3ab80
loremedda <98110186+loremedda@users.noreply.github.com>
2023-07-19 14:44:42 +0200

    SELC-2626 Replacing the deprecated API with the new one
    * SELC-2626 Replacing the deprecated API with the new one
    
    * SELC-2626 Temp commit
    
    * SELC-2626 Writing a regexp to solve problems related to model generation

--- openApi/scripts/api-b4f-onboarding-pnpg_fixPostGen.js
+++ openApi/scripts/api-b4f-onboarding-pnpg_fixPostGen.js
@@ -3,3 +3,16 @@ const regexReplace = require('regex-replace');
 regexReplace('array', 'Array<string>', 'src/api/generated/b4f-onboarding-pnpg/requestTypes.ts', {
   fileContentsOnly: true,
 });
+
+regexReplace(
+  `body\\?\\: File`,
+  `body: string | Blob`,
+  'src/api/generated/b4f-onboarding-pnpg/requestTypes.ts',
+  {
+    fileContentsOnly: true,
+  }
+);
+
+regexReplace(`body_`, `body`, 'src/api/generated/b4f-onboarding-pnpg/client.ts', {
+  fileContentsOnly: true,
+});
