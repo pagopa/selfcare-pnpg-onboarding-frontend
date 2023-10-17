7b319365172b9395ab50f7a91fd95b15e70e0b0d
loremedda <98110186+loremedda@users.noreply.github.com>
2023-10-17 15:58:53 +0200

    SELC-3242 Updated translation
    * SELC-3242 Updated translations
    
    * SELC-3242 Updated common dependency at the most recent version

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,4 +1,8 @@
 import i18n, { configureI18n } from '@pagopa/selfcare-common-frontend/locale/locale-utils';
 import it from './it';
+import en from './en';
+import de from './de';
+import fr from './fr';
+import sl from './sl';
 
-configureI18n({ i18n, it });
+configureI18n({ i18n, it, en, de, fr, sl });
